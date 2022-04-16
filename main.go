65e724a912bf4e0a27537404bb276944c553c0b7
leon ferdian <leonardoferdiano@gmail.com>
2022-04-16 11:45:33 +0700

    create function login
    create function login on progress

--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+/*
+ * Tag... - a very simple struct
+ */
+type Tag struct {
+	Name     string `json:"name"`
+	Password string `json:"password"`
+}
 
 func main() {
-    fmt.Println("Hello, World!")
+	id := 1
+	username := "Admin 1"
+	login(id, username)
+}
+
+func login(id int, username string) {
+	// Open up our database connection.
+	// I've set up a database on my local machine using phpmyadmin.
+	// The database is called testDb
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/majoo")
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		log.Print(err.Error())
+	}
+	defer db.Close()
+
+	// Execute the query
+	results, err := db.Query("select name, password from users where name = ?", username)
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	for results.Next() {
+		var tag Tag
+		// for each row, scan the result into our tag composite object
+		err = results.Scan(&tag.Name, &tag.Password)
+		if err != nil {
+			panic(err.Error()) // proper error handling instead of panic in your app
+		}
+		// and then print out the tag's Name attribute
+		log.Printf(tag.Name, tag.Password)
+	}
+
+	fmt.Println("Login Success")
 }
